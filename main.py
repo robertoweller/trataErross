dc0b1c7f9389feb3c428ddde558f24027570e87a
roberto <robertoweller78@gmail.com>
2020-02-11 23:16:41 -0300

    Codigo melhorado preciso estudar uma forma de pegar linha a linha e jogar para dentro de uma variavel.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+
 try:
 
     x = '''
-print (z)
+
+
+print '1234567891011121314151617181920'
 
 print '0'
 
@@ -13,20 +16,28 @@ except SyntaxError as erro:
     # Retorna a mensagem de erro em str.
     mensage = erro.msg
     # retorna o print()...S2
-    pri = erro.msg[53:63]
+    pri = erro.msg[53:64]
     # Me retorna o codigo errado em str
     texto = erro.text
     # Retorna a linha do erro em int
     linha = erro.lineno
     # Retorna quantas letras tem o codigo errado em int
     letras = erro.offset
-    # retorna o que tipo é + estudo n entedi direito
+    # retorna o que tipo é, tem que estudar + n entedi direito
     sla = erro.filename
 
-    test = pri
+    # Me retorna o print corrrigido, dentro da variavel test em forma de str.
+    aaa = len(mensage)
+    test = mensage[53:aaa-1] # Fatia a area do print('') junto com o texto entre ''
 
-    print(f'{test} é do tipo >> {type(test)}')
-    print(f'{mensage} Erro na linha {linha}...')
-    x = pri
-    print(x)
-    exec(x)
+    # O erro tem 63 letras contando de 0.
+    print(f'{test} é do tipo >> {type(test)} \n')
+    print('-----------Apartir daqui o codigo corrigido------------------------')
+
+    x = x.replace(texto, test)
+
+    print(f'\n{x}')
+
+
+# Deixa para executar o codigo depois
+#    exec(x)
